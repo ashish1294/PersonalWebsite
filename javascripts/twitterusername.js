b4a8d611154fbdcb391e6d3b593c51e1017ac442
Ashish Kedia <ashish1294@gmail.com>
2015-03-16 17:25:03 +0530

    Removed the Old Simple Website. Moved to Django

--- javascripts/twitterusername.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($){
-        $(".tweet").tweet({
-            username: "twitter",
-            avatar_size: 50,
-            count: 1,
-            loading_text: "loading tweets..."
-        });
-    });
