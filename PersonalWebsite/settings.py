22067f3bfc4e3b101ca9da659a215b0bb669021f
Ashish kedia <ashish1294@gmail.com>
2015-05-29 14:11:39 -0700

    Skill Chart Files Added !
    Settings Edited !!

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'personalwebsite',
         'USER': 'root',
-        'PASSWORD': 'sumimenpc',
+        'PASSWORD': 'test_password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
